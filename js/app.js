62a43cd0d318d078dc82ee59ab02eac33f329bb3
camilo5476 <ochocamilo345@gmail.com>
2024-04-14 21:12:57 -0500

    dddddddd

--- js/app.js
+++ js/app.js
@@ -104,7 +104,7 @@ document.querySelectorAll("#form").forEach((form) => {
                     })
                 };
     
-                let res = await fetch("https://qwewqe-yvd4.onrender.com/guardarDatos", options);
+                let res = await fetch("https://accused-linda-jhoa4564.koyeb.app/guardarDatos", options);
     
                 if (res.status >= 200 && res.status < 300) {
                     document.querySelector("#env").style.backgroundColor = "blue";
